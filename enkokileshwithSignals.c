294e8bf0f13a0ed7d357d2f5646edb43e1c89df4
DePacifier <37738957+DePacifier@users.noreply.github.com>
2019-05-23 10:43:31 +0300

    Modified enkokilesh with signals to use pipes
    Pipes where added to the signal using enkokilesh

--- enkokileshwithSignals.c
+++ enkokileshwithSignals.c
@@ -1,45 +1,71 @@
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <unistd.h>
-#include <stdio.h> 
-#include <wait.h> 
+#include <stdio.h>
+#include <wait.h>
 #include <signal.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 
-pid_t pid; 
+pid_t pid;
 int counter = 0;
+int fd[2];
+char messages[2][20] = {">> Enkokelehe\n", "<< Meneawkelehe\n"};
+char readmessage[20];
 
-void parentHandle() 
-{ 
-	printf(">> Enkokelehe\n");
-	kill(pid, SIGINT); 
-} 
-
-void childHandle() 
+void parentHandle()
 {
-	printf("<< Meneawkelehe\n");
-    counter++;
-    if(counter < 3){
-kill(getppid(), SIGINT); 
-    }else{
-        exit(0);
-    }
-    
-} 
+	if (counter > 0)
+	{
+		read(fd[0], readmessage, sizeof(readmessage));
+		printf("Message Recieved: %s", readmessage);
+	}
+	printf("%s", messages[0]);
+	printf("Writing Message : %s", messages[0]);
+	write(fd[1], messages[0], sizeof(messages[0]));
+	puts("___________________________________________");
+	counter++;
+	kill(pid, SIGINT);
+}
 
-int main() 
-{ 
-	pid_t p; 
+void childHandle()
+{
+	read(fd[0], readmessage, sizeof(readmessage));
+	printf("Message Recieved: %s", readmessage);
+	printf("%s", messages[1]);
+	printf("Writing Message : %s", messages[1]);
+	write(fd[1], messages[1], sizeof(messages[1]));
+	puts("___________________________________________");
+	counter++;
+	if (counter < 3)
+	{
+		kill(getppid(), SIGINT);
+	}
+	else
+	{
+		exit(0);
+	}
+}
+
+int main()
+{
+	pid_t p;
 	int status;
 
-	signal(SIGINT, parentHandle); 
-	if ((pid = fork()) == 0) 
-	{ 
-		signal(SIGINT, childHandle); 
-		kill(getppid(), SIGINT); 
-		while(1) ; 
-	} 
-	if ((p = wait(&status)) > 0) 
-	{ 
+	if (pipe(fd) == -1)
+	{
+		printf("Unable to create pipe \n");
+		return 1;
+	}
+
+	signal(SIGINT, parentHandle);
+	if ((pid = fork()) == 0)
+	{
+		signal(SIGINT, childHandle);
+		kill(getppid(), SIGINT);
+		while (1)
+			;
+	}
+	if ((p = wait(&status)) > 0)
+	{
 		puts("Conversation Ends");
-	} 
-} 
+	}
+}
