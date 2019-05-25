03f379567c18fa64ba2245e8666c8d8abe85d892
Tewodros Kederalah <teddylnk1@gmail.com>
2019-05-25 06:44:43 +0000

    Used two pipes and signals to create a back and forth communication between the two processes

--- enkokileshe.c
+++ enkokileshe.c
@@ -1,42 +1,88 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <stdlib.h>
+// C program to implement sighup(), sigint() 
+// and sigquit() signal functions 
+#include <signal.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <sys/types.h> 
+#include <unistd.h> 
+  
+// function declaration 
+void sighup(); 
+void sigint(); 
 
 
-int main()
-{
-    int fd[2];
-    char messages[2][20] = {"enkokileshe", "Meneawkelehe"};
-    char readmessage[20];
+int fd[2], fd1[2], pid;
+char responder[2][13] = {"Meneawkelehe","Bet"};
+char asker[3][28] = {"Enkokileshe", "Zoro Zoro megbiyaw cheraro?", "Melsu -> Asphalt"};
+char readmessage[28];  
 
+// driver code 
+void main() 
+{ 
+    int pid; 
     if (pipe(fd) == -1)
     {
         printf("Unable to create pipe \n");
-        return 1;
+        exit(1);
     }
-
-    for (int i = 0; i < 3; i++)
-    {
-        if (fork() != 0)
-        {                       // Parent process
-            printf("In Parent: Writing to pipe – Message is %s\n", messages[0]);
-            write(fd[1], messages[0], sizeof(messages[0]));
-            wait(NULL);
-            read(fd[0], readmessage, sizeof(readmessage));
-            printf("In Parent: Reading from pipe – Message is %s\n", readmessage);
-        }
-        else
-        {                       //child process
-            read(fd[0], readmessage, sizeof(readmessage));
-            printf("In Child: Reading from pipe – Message is %s\n", readmessage);
-            printf("In Child: Writing to pipe – Message is %s\n", messages[1]);
-            write(fd[1], messages[1], sizeof(messages[1]));
-            exit(0);
-        }
+    if(pipe(fd1) == -1){
+        printf("Unable to create pipe\n");
+        exit(1);
     }
-
-    return 0;
-}
+  
+    /* get child process */
+    if ((pid = fork()) < 0) { 
+        perror("fork"); 
+        exit(1); 
+    } 
+  
+    if (pid == 0) { /* child */
+        signal(SIGHUP, sighup); 
+        signal(SIGINT, sigint);
+        for (;;) 
+            ; /* loop for ever */
+    } 
+  
+    else /* parent */
+    { /* pid hold id of child */
+        printf("In Parent: Writing to pipe – Message is %s\n", asker[0]);    // Said Enkokelesh
+        write(fd[1], asker[0], sizeof(asker[0]));
+        kill(pid, SIGHUP); 
+  
+        sleep(3); /* pause for 3 secs */
+        read(fd1[0], readmessage, sizeof(readmessage));
+        printf("In Parent: Reading from pipe – Message is %s\n", readmessage);
+        printf("In Parent: Writing to pipe - Message is %s\n", asker[1]);
+        write(fd[1], asker[1], sizeof(asker[1])); 
+        kill(pid, SIGINT); 
+  
+        sleep(3); /* pause for 3 secs */
+        read(fd1[0], readmessage, sizeof(readmessage));
+        printf("In Parent: Reading from pipe - Message is %s\n", readmessage);
+        printf("In Parent: Writing to pipe - Message is %s\n", asker[2]);
+        write(fd[1], asker[2], sizeof(asker[2]));
+         
+    } 
+} 
+  
+// sighup() function definition 
+void sighup() 
+  
+{ 
+    signal(SIGHUP, sighup); /* reset signal */
+    read(fd[0], readmessage, sizeof(readmessage));
+    printf("In Child: Reading from pipe – Message is %s\n", readmessage);
+    printf("In Child: Writing to pipe – Message is %s\n", responder[0]);
+    write(fd1[1], responder[0], sizeof(responder[0]));
+} 
+  
+// sigint() function definition 
+void sigint() 
+  
+{ 
+    signal(SIGINT, sigint); /* reset signal */
+    read(fd[0], readmessage, sizeof(readmessage));
+    printf("In Child: Reading from pipe - Message is %s\n", readmessage);
+    printf("In Child: Writing to pipe - Message is %s\n", responder[1]);
+    write(fd1[1], responder[1], sizeof(responder[1]));
+} 
